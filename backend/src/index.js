3a52a4f3fc23b803a44675de2b3fb2065eef925d
unknown <breno_lopes_10@yahoo.com.br>
2020-04-01 15:32:52 -0300

    validação de rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-const express= require('express');
-const routes = require('./routes');
-const cors = require('cors');
+const express = require("express");
+const { errors } = require("celebrate");
+const routes = require("./routes");
+const cors = require("cors");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(3001, ()=> console.log('Server running in port 3001'));
+app.use(errors());
+app.listen(3001, () => console.log("Server running in port 3001"));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,88 @@
 const { Router } = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 const routes = Router();
 const OngController = require("./Controllers/OngController");
 const IncidentController = require("./Controllers/IncidentController");
 const ProfileController = require("./Controllers/ProfileController");
 const SessionController = require("./Controllers/SessionController");
 
-routes.post("/ongs", OngController.store);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.store
+);
+
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
 routes.get("/ongs", OngController.index);
-routes.post("/incidents", IncidentController.store);
-routes.get("/incidents", IncidentController.index);
-routes.delete("/incidents/:id", IncidentController.delete);
-routes.get("/profile", ProfileController.index);
-routes.post("/sessions", SessionController.create);
+
+routes.post(
+  "/incidents",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string()
+        .required()
+        .required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  IncidentController.store
+);
+
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
+
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
+
+routes.post(
+  "/sessions",
+  celebrate({
+    [Segments.BODY]: {
+      id: Joi.required()
+    }
+  }),
+  SessionController.create
+);
 module.exports = routes;
